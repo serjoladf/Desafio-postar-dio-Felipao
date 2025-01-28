d0ffde0fb15fdd93430ba4cf5ec7c6323a9bd720
serjoladf <serjolasrg@gmail.com>
2025-01-28 09:34:31 -0300

    desafio projeto Felipão

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int forcaXP = 0;
+        String forca;
+        System.out.println("Digite um nome do seu Heroi;");
+        String nome = sc.nextLine();
+
+        do {
+            System.out.println("Digite o XP do seu Heroi;");
+            forcaXP = sc.nextInt();
+
+        }while (forcaXP < 0) ;
+
+        if(forcaXP < 1000 ){
+            forca = ("Ferro");
+        }else if(forcaXP > 1001 && forcaXP < 2000){
+            forca = ("Bronze");
+        }else if(forcaXP > 2001 && forcaXP < 5000){
+            forca = ("Prata");
+        }else if(forcaXP > 5001 && forcaXP < 7000){
+            forca = ("Ouro");
+        }else if(forcaXP > 7001 && forcaXP < 8000){
+            forca = ("Platina");
+        }else if(forcaXP > 8001 && forcaXP < 9000){
+            forca = ("Ascendente");
+        }else if(forcaXP > 9001 && forcaXP < 10000){
+            forca = ("Imortal");
+        }else{
+            forca = ("Super saiyajin Radiante");
+        }
+        System.out.printf("O Herói de nome %s esta no nível de %s.",nome,forca);
+    }
+}
